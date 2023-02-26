2fbbe3c9ad108abe7cf60c3aacc1059ace4f66b7
Prathamesh Mali <prathamesh.mali@mosaicwellness.in>
2023-02-27 03:44:39 +0530

    feat: 🎸 updated schemas for documents

---
+++ schemas/contacts/contacts.schema.ts
@@ -0,0 +1,52 @@
+import {defineType, defineField} from 'sanity'
+
+const links = defineType({
+  name: 'link',
+  title: 'Link',
+  type: 'array',
+  of: [
+    {
+      type: 'link',
+    },
+  ],
+})
+
+const link = defineType({
+  title: 'link',
+  name: 'Links',
+  type: 'object',
+  fields: [
+    {
+      name: 'socialLink',
+      title: 'Social Link',
+      type: 'string',
+    },
+    {
+      name: 'link1',
+      title: 'Link1',
+      type: 'string',
+    },
+  ],
+})
+
+const contactsLink = defineType({
+  name: 'contactsLink',
+  title: 'Contacts Link',
+  type: 'object',
+
+  fields: [
+    {
+      name: 'socialLink',
+      title: 'Social Link',
+      type: 'string',
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'string',
+    },
+  ],
+})
+
+export {links, link, contactsLink}
+// export {link}
